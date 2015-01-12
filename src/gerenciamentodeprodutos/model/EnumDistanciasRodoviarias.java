52d44943e6f665aa94e2fcbbed8a6fe6973789ae
Anderson <Anderson@QUADCORE>
2015-01-12 00:06:25 -0200

    Tentando implementar método que retorna distância contida no enum EnumDistanciasRodoviarias

--- src/gerenciamentodeprodutos/model/EnumDistanciasRodoviarias.java
+++ src/gerenciamentodeprodutos/model/EnumDistanciasRodoviarias.java
@@ -18,12 +18,36 @@ public enum EnumDistanciasRodoviarias {
     BELOHORIZONTECURITIBA(820);
 
     private final int distancia;
+    private String distancias[];
 
     EnumDistanciasRodoviarias(int distancia) {
-        this.distancia = distancia;
+      this.distancia = distancia;
     }
 
     public int getDistancia() {
-        return this.distancia;
+        //return this.distancia;
+        return 0;
     }
+
+    //public int distanciaEntreAsCidades(String cidade1, String cidade2) {
+        public int distanciaEntreAsCidades() {
+        //String c1 = cidade1;
+        //String c2 = cidade2;
+        
+        String c1 = "Recife";
+        String c2 = "BeloHorizonte";
+            
+        String juncao1 = c1 + c2;
+        juncao1 = juncao1.toUpperCase();
+
+        String juncao2 = c2 + c1;
+        juncao2 = juncao2.toUpperCase();
+        
+        System.out.println(juncao1);
+        System.out.println(juncao2);
+
+        //return distancia;
+        return 0;
+    }
+
 }
